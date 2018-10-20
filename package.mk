5ea7428073e77e129eef6a27624e500dd91c9276
Persian Prince <persianpros@yahoo.com>
2018-10-20 11:00:14 +0330

    Update package.mk

--- package.mk
+++ package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="rpi-e2-procfs"
-PKG_VERSION="4.14.76"
+PKG_VERSION="4.14.77"
 PKG_REV="1"
 PKG_ARCH="arm"
 PKG_LICENSE="nonfree"

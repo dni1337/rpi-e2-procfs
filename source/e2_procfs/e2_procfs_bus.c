c7578eeb3b68b10817d1da0ba6f46bae78c98165
dni1337 <15782082+dni1337@users.noreply.github.com>
2020-02-03 10:47:35 +0100

    Update e2_procfs_bus.c

--- source/e2_procfs/e2_procfs_bus.c
+++ source/e2_procfs/e2_procfs_bus.c
@@ -71,7 +71,7 @@ int e2procfs_nim_sockets_show(struct seq_file *m, void* data)
 		else	{
  			while (adapter_num < DVB_MAX_ADAPTERS)
  			{
- 				int frontend_num = 0;
+ 				int frontend_num = 1;
 
 	 			while (frontend_num < DVB_MAX_FRONTEND)
  				{
@@ -87,13 +87,13 @@ int e2procfs_nim_sockets_show(struct seq_file *m, void* data)
  						struct dtv_properties cmdseq = { .num = 1, .props = p };
  						if (dvb_generic_ioctl(fe_fd, FE_GET_INFO, 0))
  						{
- 							//dvbdev->kernel_ioctl(fe_fd, FE_GET_INFO, &fe_info);
+ 							dvbdev->kernel_ioctl(fe_fd, FE_GET_INFO, &fe_info);
  							if (dvb_generic_ioctl(fe_fd, FE_GET_PROPERTY, 0))
  							{
- 								//dvbdev->kernel_ioctl(fe_fd, FE_GET_PROPERTY, &cmdseq);
+ 								dvbdev->kernel_ioctl(fe_fd, FE_GET_PROPERTY, &cmdseq);
 
 /* 	2nd generation DVB Tuner detected adding 2 to the TunerType */
-								/*if ( (fe_info.caps & FE_CAN_2G_MODULATION ) == FE_CAN_2G_MODULATION )
+								if ( (fe_info.caps & FE_CAN_2G_MODULATION ) == FE_CAN_2G_MODULATION )
 								{
 									printk("procfs debug 8\n");
  									seq_printf(m,
@@ -114,21 +114,21 @@ int e2procfs_nim_sockets_show(struct seq_file *m, void* data)
 								else	{*/
 									seq_printf(m,
 	 								"NIM Socket %d:\n"
- 									//"\tType: %s\n"
-	 								//"\tName: %s\n"
- 									"\tType: DVB-S2\n"
-	 								"\tName: USB Tuner\n"
+ 									"\tType: %s\n"
+	 								"\tName: %s\n"
+ 									//"\tType: DVB-S2\n"
+	 								//"\tName: USB Tuner\n"
  									"\tHas_Outputs: no\n"
  								//	"\tInternally_Connectable: 0\n"
  									"\tFrontend_Device: %d\n"
  								//	"\tI2C_Device: -1\n"
  									,
 	 								nsocket_index,
- 									//DVBTunerType[fe_info.type],
- 									//fe_info.name,
+ 									DVBTunerType[fe_info.type],
+ 									fe_info.name,
  									frontend_num
  									);
-								//}
+								}
 	
 								nsocket_index++; 					
  							}

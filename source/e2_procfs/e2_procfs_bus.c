5ab826d7a9ac0d6add3eae0ee4c0f486e35550a0
dni1337 <15782082+dni1337@users.noreply.github.com>
2020-02-03 15:36:49 +0100

    Update e2_procfs_bus.c

--- source/e2_procfs/e2_procfs_bus.c
+++ source/e2_procfs/e2_procfs_bus.c
@@ -55,7 +55,7 @@ int e2procfs_nim_sockets_show(struct seq_file *m, void* data)
 	else
 	{
  		struct file* fe_fd = NULL;
- 		int adapter_num = 1, nsocket_index = 0;
+ 		int adapter_num = 0, nsocket_index = 0;
  		char devstr[MAX_CHAR_LENGTH];
 
 /*	User Space nim_socket  (not enabled by default) */
@@ -77,7 +77,7 @@ int e2procfs_nim_sockets_show(struct seq_file *m, void* data)
  				{
  					int bytes = 0;
  					bytes = sprintf(devstr, "/dev/dvb/adapter%d/frontend%d", adapter_num, frontend_num);
- 					fe_fd = file_open(devstr, O_RDONLY, bytes);
+ 					fe_fd = file_open(devstr, O_RDWR, bytes);
 
  					if (fe_fd != NULL)
  					{
@@ -87,13 +87,13 @@ int e2procfs_nim_sockets_show(struct seq_file *m, void* data)
  						struct dtv_properties cmdseq = { .num = 1, .props = p };
  						if (dvb_generic_ioctl(fe_fd, FE_GET_INFO, 0))
  						{
- 							//dvbdev->kernel_ioctl(fe_fd, FE_GET_INFO, &fe_info);
+ 							dvbdev->kernel_ioctl(fe_fd, FE_GET_INFO, &fe_info);
  							if (dvb_generic_ioctl(fe_fd, FE_GET_PROPERTY, 0))
  							{
- 								//dvbdev->kernel_ioctl(fe_fd, FE_GET_FRONTEND, &cmdseq);
+ 								dvbdev->kernel_ioctl(fe_fd, FE_GET_FRONTEND, &cmdseq);
 
 /* 	2nd generation DVB Tuner detected adding 2 to the TunerType */
-								/*if ( (fe_info.caps & FE_CAN_2G_MODULATION ) == FE_CAN_2G_MODULATION )
+								if ( (fe_info.caps & FE_CAN_2G_MODULATION ) == FE_CAN_2G_MODULATION )
 								{
 									printk("procfs debug 8\n");
  									seq_printf(m,
@@ -111,24 +111,24 @@ int e2procfs_nim_sockets_show(struct seq_file *m, void* data)
  									frontend_num
  									);
 		 						}	
-								else	{*/
+								else	{
 									seq_printf(m,
 	 								"NIM Socket %d:\n"
- 									//"\tType: %s\n"
-	 								//"\tName: %s\n"
- 									"\tType: DVB-S2\n"
-	 								"\tName: USB Tuner\n"
+ 									"\tType: %s\n"
+	 								"\tName: %s\n"
+ 									//"\tType: DVB-S2\n"
+	 								//"\tName: USB Tuner\n"
  									"\tHas_Outputs: no\n"
  								//	"\tInternally_Connectable: 0\n"
  									"\tFrontend_Device: %d\n"
  								//	"\tI2C_Device: -1\n"
  									,
 	 								nsocket_index,
- 									//DVBTunerType[fe_info.type],
- 									//fe_info.name,
+ 									DVBTunerType[fe_info.type],
+ 									fe_info.name,
  									frontend_num
  									);
-								//}
+								}
 	
 								nsocket_index++; 					
  							}

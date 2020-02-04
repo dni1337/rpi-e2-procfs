77d5d4756350954b8fa8b2d9f8f73387a84fa098
dni1337 <15782082+dni1337@users.noreply.github.com>
2020-02-04 16:14:58 +0100

    Update e2_procfs_bus.c

--- source/e2_procfs/e2_procfs_bus.c
+++ source/e2_procfs/e2_procfs_bus.c
@@ -85,7 +85,7 @@ int e2procfs_nim_sockets_show(struct seq_file *m, void* data)
  						struct dvb_frontend_info fe_info;
  						struct dtv_property p[] = {{ .cmd = DTV_DELIVERY_SYSTEM }};
  						struct dtv_properties cmdseq = { .num = 1, .props = p };
-						
+						printk("dvb_fe_info: %i", FE_GET_INFO);
 						printk("dvb_hello: %ld", dvb_generic_ioctl(fe_fd, FE_GET_INFO, 0));
 						
  						//if (dvb_generic_ioctl(fe_fd, FE_GET_INFO, 0))
